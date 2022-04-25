7533d077d2b7f46cfbbf8295495d788d8abdfb84
SherlockYigit <sherlockyigit.dev@gmail.com>
2022-04-25 17:49:25 +0300

    update: Youtube search added.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/SherlockYigit/kommando"
 	"github.com/SherlockYigit/youtube-go"
 	"github.com/manifoldco/promptui"
+	"strings"
 )
 
 func main() {
@@ -16,5 +17,58 @@ func main() {
 		FlagListTemplate:    "--{FlagName} {FlagDescription}",
 	})
 
+	handler.AddCommand(kommando.Command{
+		Name:        "download",
+		Description: "It searches and downloads the relevant file according to the selected result.",
+		Flags: []kommando.Flag{
+			kommando.Flag{
+				Name:        "path",
+				Description: "Location to save the downloaded file.",
+			},
+		},
+		Execute: func(Res kommando.CommandResponse) {
+			var squery string
+			var flags []string
+
+			for i := 0; len(Res.Args) > i; i++ {
+				val := Res.Args[i]
+
+				if !strings.Contains(val, "=") {
+					squery = val
+				} else {
+					flags = append(flags, val)
+				}
+			}
+
+			res := youtubego.Search(squery, youtubego.SearchOptions{
+				Type:  "video",
+				Limit: 15,
+			})
+
+			prompt := promptui.Select{
+				Label: "Select video",
+				Items: getTitles(res),
+			}
+
+			selectedIndex, _, err := prompt.Run()
+
+			if err != nil {
+				panic("Prompt failed " + err.Error())
+			}
+
+			fmt.Println(res[selectedIndex].Title)
+		},
+	})
+
 	handler.Run()
+}
+
+func getTitles(val []youtubego.SearchResult) []string {
+	var titles []string
+
+	for i := 0; len(val) > i; i++ {
+		titles = append(titles, val[i].Video.Title)
+	}
+
+	return titles
 }
